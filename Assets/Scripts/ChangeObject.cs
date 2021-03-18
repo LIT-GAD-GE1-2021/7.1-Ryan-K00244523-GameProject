fc14946dd68316f79b210864672c0b2306cce280
LIT_Ryan <77435288+RyanLim1115@users.noreply.github.com>
2021-03-18 09:30:42 +0000

    Add_In_SomeAssets

---
+++ Assets/Scripts/ChangeObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeObject : MonoBehaviour
+{
+    public GameObject summonGameObject;
+    public bool createObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        createObject = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( createObject == true)
+        {
+            GameObject newTutorial;
+            newTutorial = Instantiate(summonGameObject, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.identity);
+            createObject = false;
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider == true)
+        {
+            createObject = true;
+
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+            createObject = true;
+    }
+
+}
