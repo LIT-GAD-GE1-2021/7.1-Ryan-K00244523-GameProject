a78877d377faa1e546726f82e12e47f390719417
LIT_Ryan <77435288+RyanLim1115@users.noreply.github.com>
2021-04-07 22:33:32 +0100

    CharacterGolemController

--- Assets/Scripts/CharacterGolemScript.cs
+++ Assets/Scripts/CharacterGolemScript.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class CharacterGolemScript : MonoBehaviour
 {
+    public float jumpSpeed;
+    public float fallMultiplier;
+    public float lowJumpMultiplier;
+    public float horizontalSpeed;
+
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+    public Transform headCheck;
+    private float groundRadius;
+    private float headRadius;
+    private bool grounded;
+    private bool headTouch;
+    private float hAxis;
+    private bool facingRight;
+
+    private bool jump;
+
+    private Rigidbody2D golemRigidbody;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        jump = false;
+        facingRight = true;
+
+        golemRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        jump = Input.GetKeyDown(KeyCode.Space);
+        hAxis = Input.GetAxis("Horizontal");
+
+        Collider2D colliderWeCollidedWith = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+        grounded = (bool)colliderWeCollidedWith;
+
+        if (grounded)
+        {
+            if ((hAxis>0) && (facingRight == false))
+            {
+                Flip();
+            }
+            else if ((hAxis<0) && (facingRight == true))
+            {
+                Flip();
+            }
+
+        }
+    }
+
+    void FixedUpdate()
+    {
         
     }
+
+    private void Flip()
+    {
+
+    }
+
 }
