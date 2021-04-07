8ac30a667053732560eccac0b1e5e564910465ad
LIT_Ryan <77435288+RyanLim1115@users.noreply.github.com>
2021-04-07 23:48:14 +0100

    CharacterGolem_Ctrl Fix2

--- Assets/Scripts/CharacterGolemScript.cs
+++ Assets/Scripts/CharacterGolemScript.cs
@@ -7,12 +7,12 @@ public class CharacterGolemScript : MonoBehaviour
     public float jumpSpeed;
     public float fallMultiplier;
     public float lowJumpMultiplier;
-    public float horizontalSpeed;
+    public float horizontalSpeed = 10;
 
     public LayerMask whatIsGround;
     public Transform groundCheck;
     public Transform headCheck;
-    private float groundRadius;
+    private float groundRadius = 0.5f;
     private float headRadius;
     private bool grounded;
     private bool headTouch;
@@ -29,6 +29,7 @@ public class CharacterGolemScript : MonoBehaviour
     void Start()
     {
         jump = false;
+        grounded = false;
         facingRight = true;
 
         golemRigidbody = GetComponent<Rigidbody2D>();
@@ -44,23 +45,43 @@ public class CharacterGolemScript : MonoBehaviour
 
         grounded = (bool)colliderWeCollidedWith;
 
+        float yVelocity = golemRigidbody.velocity.y;
+
         if (grounded)
         {
-            if ((hAxis>0) && (facingRight == false))
+            if ((hAxis > 0) && (facingRight == false))
             {
                 Flip();
             }
-            else if ((hAxis<0) && (facingRight == true))
+            else if ((hAxis < 0) && (facingRight == true))
             {
                 Flip();
             }
 
         }
+        if (grounded && !jump)
+        {
+            golemRigidbody.velocity = new Vector2(horizontalSpeed * hAxis, golemRigidbody.velocity.y);
+        }
+        else if (grounded && jump)
+        {
+            golemRigidbody.velocity = new Vector2(golemRigidbody.velocity.x, jumpSpeed);
+        }
+
+        if (golemRigidbody.velocity.y < 0)
+        {
+            golemRigidbody.velocity += Vector2.up * Physics2D.gravity.y * fallMultiplier * Time.deltaTime;
+        }
+        else if ((golemRigidbody.velocity.y > 0) && (!Input.GetKey(KeyCode.Space)))
+        {
+            golemRigidbody.velocity += Vector2.up * Physics2D.gravity.y * lowJumpMultiplier * Time.deltaTime;
+        }
     }
 
     void FixedUpdate()
     {
-        
+
+
     }
 
     private void Flip()
